4e792d797d307f3c8b4198849186f32a5862ef5c
Jos Castro <joselycas@gmail.com>
2018-08-14 13:20:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,7 +84,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(grade)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
     SELECT * FROM students WHERE grade = ?
     LIMIT 2
